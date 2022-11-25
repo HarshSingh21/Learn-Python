0e5edb2f5f9f252ae5944082f1eac7089d09be87
HarshSingh21 <singhharsh8283@gmail.com>
2022-11-26 00:50:45 +0530

    file_type

---
+++ Basic/type.py
@@ -0,0 +1,12 @@
+rating = float(4.7)
+print(rating,type(rating))
+name = 'harsh'
+print(name,type(name))
+marks = 98
+print(marks,type(mark
+is_passed = True
+print(is_passed,type(is_passed))
+my_complex_number = 4+ 5j
+print(my_complex_number,type(my_complex_number))
+fruit_list = ['apple','banana','mango']
+print(fruit_list,type(fruit_list))

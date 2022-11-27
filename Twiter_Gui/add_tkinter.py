94ccfcf65b75058794ca82b59a69bd8d97b5f2e0
Harsh Singh <94156692+HarshSingh21@users.noreply.github.com>
2022-11-28 00:00:59 +0530

    Update add_tkinter.py

--- Twiter_Gui/add_tkinter.py
+++ Twiter_Gui/add_tkinter.py
@@ -10,6 +10,7 @@ class MyWindow:
         self.t3=Entry()
         
         
+        
         self.btn1 = Button(win, text='Add')
         self.btn2=Button(win, text='Subtract')
         self.lbl1.place(x=100, y=50)
